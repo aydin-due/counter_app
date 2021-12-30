bb96503e96a04ed8c92381d86c695ca4b451cc9c
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2021-12-30 06:56:26 -0700

    floatingActionButton icon

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const fontSize30 = const TextStyle(fontSize: 30);
+    const fontSize30 = TextStyle(fontSize: 30);
 
     return Scaffold(
       appBar: AppBar(
@@ -21,6 +21,12 @@ class HomeScreen extends StatelessWidget {
             Text('0', style: fontSize30)
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          print('Hola Mundo');
+        },
       ),
     );
   }
